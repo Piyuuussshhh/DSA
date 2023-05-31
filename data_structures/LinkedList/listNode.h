b13108f13585273c2684f0504a6f837bf5fb1bb3
Piyush Udhao <pudhao12@gmail.com>
2023-05-31 11:49:36 +0530

    added two functions to linked list, display and creation from an array. Also made a menu in main.

--- data_structures/LinkedList/listNode.h
+++ data_structures/LinkedList/listNode.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <iostream>
 
-template <class T>
+// * Declaration
+template <class T = int>
 class ListNode {
 private:
     T data;
@@ -14,8 +15,10 @@ public:
     void setNext(ListNode *node);
 };
 
+// * Implementation
+
 template <class T>
-ListNode<T>::ListNode(T d) : data(d) {}
+ListNode<T>::ListNode(T d) : data(d), next(NULL) {}
 
 template <class T>
 ListNode<T>::~ListNode() {}
@@ -34,3 +37,32 @@ template <class T>
 void ListNode<T>::setNext(ListNode *node) {
     this -> next = node;
 }
+
+// * Linked List Functions
+
+// Prints a linked list.
+template <class T>
+void displayLinkedList(ListNode<T> *head) {
+    ListNode<T> *temp = head;
+    while (temp != nullptr) {
+        std::cout << temp -> getData() << " ";
+        temp = temp -> getNext();
+    }
+}
+
+// Creates a linked list from an array of elements.
+template <class T>
+ListNode<T>* createLinkedListFromArray(T arr[], int size) {
+
+    ListNode<T> *head = new ListNode<T>(arr[0]);
+    ListNode<T> *temp, *last;
+    last = head;
+
+    for (int i = 1; i < size; i++) {
+        temp = new ListNode<T>(arr[i]);
+        last -> setNext(temp);
+        last = temp;
+    }
+
+    return head;
+};
--- main.cpp
+++ main.cpp
@@ -3,12 +3,59 @@
 #include "data_structures/LinkedList/listNode.h"
 
 int main() {
-    ListNode<int> *head = new ListNode<int>(10);
-    head -> setNext(new ListNode<int>(12));
-    head -> getNext() -> setNext(new ListNode<int>(14));
-    head -> getNext() -> getNext() -> setNext(new ListNode<int>(16));
+    // ListNode<int> *head = new ListNode<int>(10);
+    // head -> setNext(new ListNode<int>(12));
+    // head -> getNext() -> setNext(new ListNode<int>(14));
+    // head -> getNext() -> getNext() -> setNext(new ListNode<int>(16));
 
-    for (auto i = head; i != NULL; i = i -> getNext()) {
-        std::cout << i -> getData() << " ";
-    } std::cout << std::endl;
+    // for (auto i = head; i != NULL; i = i -> getNext()) {
+    //     std::cout << i -> getData() << " ";
+    // } std::cout << std::endl;
+
+    std::cout << "---------------------------MENU---------------------------" << std::endl;
+    std::cout << "1. Linked List" << std::endl;
+
+    int choice;
+    std::cout << "Enter choice: ";
+    std::cin >> choice;
+    std::cout << std::endl;
+
+    switch (choice) {
+    case 1: {
+        int listCreationChoice;
+        std::cout << "---------------------------Linked List---------------------------" << std::endl;
+        std::cout << "1. Create linked list with array." << std::endl
+                  << "2. Create linked list element-wise." << std:: endl;
+        std::cout << "Enter choice: ";
+        std::cin >> listCreationChoice;
+
+        switch (listCreationChoice) {
+        case 1: {
+            int sizeOfArray;
+            std::cout << "Enter array size: " << std::endl;
+            std::cin >> sizeOfArray;
+
+            int *arr = new int[sizeOfArray];
+
+            for (int i = 0; i < sizeOfArray; i++) {
+                std::cout << "Enter element " << i + 1 << ": ";
+                std::cin >> arr[i];
+            }
+
+            ListNode<int> *head = createLinkedListFromArray<int>(arr, sizeOfArray);
+            displayLinkedList<int>(head);
+
+            break;
+        }
+
+        default:
+            break;
+        }
+
+        break;
+    }
+
+    default:
+        break;
+    }
 }
